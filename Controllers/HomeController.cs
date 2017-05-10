5f65ddfc1779fa77abe47d5c27540988f2316c39
Perk430 <perk430@gmail.com>
2017-05-09 23:46:22 -0400

    Initial commit of Lab 20 with a small error on the summary; no name displayed

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab20_RegistrationForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+
+            ViewBag.Message = "Please fill the form to register!";
+            
+            return View();
+        }
+
+        public ActionResult AddUser(string FirstName)
+        {
+            //code to add user
+            string fName = FirstName;
+            ViewBag.FirstName = fName;
+
+            return View();
+        }
+    }
+}
